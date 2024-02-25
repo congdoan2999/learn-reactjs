fb2d30963e3ada823878ff146b2649bcfec60ebf
doan <doan29061999@gmail.com>
2024-02-25 11:36:29 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bui cong doan
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
